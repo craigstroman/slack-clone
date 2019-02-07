b8c4d09a58a5f35c798d2cec874c82f1afdf79e7
Craig Stroman <cstroman@gmail.com>
2019-02-06 23:18:23 -0500

    Working on UI

--- client/components/TeamSidebar/TeamSidebar.jsx
+++ client/components/TeamSidebar/TeamSidebar.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import './TeamSidebar.scss';
 
-const TeamSidebar = () => (
-  <div>
-    Team Sidebar
-  </div>
-);
+class TeamSidebar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.switchTeam = this.switchTeam.bind(this);
+  }
+
+  switchTeam() {
+    console.log('switchTeam: ');
+    console.log('this: ', this);
+  }
+
+  render() {
+    return (
+      <div className="teams-sidebar-container">
+        <section>
+          <div className="teams-sidebar__content">
+            <ul className="teams-list">
+              <li className="teams-list__item">
+                T
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
 
 export default TeamSidebar;
--- client/containers/Dashboard/Dashboard.jsx
+++ client/containers/Dashboard/Dashboard.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { gql, graphql } from 'react-apollo';
+import PropTypes from 'prop-types';
+// import _ from 'lodash';
+// import decode from 'jwt-decode';
 import MainSidebar from '../../components/MainSidebar/MainSidebar';
 import TeamSidebar from '../../components/TeamSidebar/TeamSidebar';
 import Messages from '../../components/Messages/Messages';
@@ -6,35 +10,72 @@ import Input from '../../components/Input/Input';
 import Header from '../../components/Header/Header';
 import './Dashboard.scss';
 
-const Dashboard = props => (
-  <div className="dashboard-container">
-    <div className="team-sidebar">
-      <TeamSidebar />
-    </div>
-    <div className="main-sidebar">
-      <MainSidebar
-        channels={[{ id: 1, name: 'general' }, { id: 2, name: 'random' }]}
-        users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
-      />
-    </div>
-    <div className="main-content">
-      <header>
-        <div className="header-container">
-          <Header {...props} />
-        </div>
-      </header>
-      <main>
-        <div className="messages-container">
-          <Messages {...props} />
-        </div>
-      </main>
-      <footer>
-        <div className="input-container">
-          <Input {...props} />
-        </div>
-      </footer>
-    </div>
-  </div>
-);
+const Dashboard = ({ data: { allTeams } }) => {
+  console.log('allTeams: ', allTeams);
 
-export default Dashboard;
+  let teams = [];
+
+  if (allTeams) {
+    teams = allTeams.map(el => ({
+      id: el.id,
+      letter: el.name.charAt(0).toUpperCase(),
+      name: el.name,
+    }));
+
+    console.log('teams: ', teams);
+  }
+
+  return (
+    <div className="dashboard-container">
+      <div className="team-sidebar">
+        <TeamSidebar teams={teams} />
+      </div>
+      <div className="main-sidebar">
+        <MainSidebar
+          channels={[{ id: 1, name: 'general' }, { id: 2, name: 'random' }]}
+          users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
+        />
+      </div>
+      <div className="main-content">
+        <header>
+          <div className="header-container">
+            <Header />
+          </div>
+        </header>
+        <main>
+          <div className="messages-container">
+            <Messages />
+          </div>
+        </main>
+        <footer>
+          <div className="input-container">
+            <Input />
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+const allTeamsQuery = gql`
+  {
+    allTeams {
+      id
+      name
+      channels {
+        id
+        name
+      }
+    }
+  }
+`;
+
+Dashboard.defaultProps = {
+  data: [],
+};
+
+Dashboard.propTypes = {
+  data: PropTypes.object,
+};
+
+export default graphql(allTeamsQuery)(Dashboard);
