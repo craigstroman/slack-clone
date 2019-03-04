5f2979031cd6a391a8976959396d3c21192df7f4
Craig Stroman <cstroman@gmail.com>
2019-03-03 22:28:08 -0500

    Refactoring some code

--- server/resolvers/team.js
+++ server/resolvers/team.js
@@ -36,29 +36,31 @@ export default {
 
         return {
           ok: false,
-          errors: formatErrors(err),
+          errors: formatErrors(err, models),
         };
       }
     }),
     createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
-        const team = await models.Team.create({ ...args, owner: user.id });
+        const response = await models.sequelize.transaction(async () => {
+          const team = await models.Team.create({ ...args, owner: user.id });
 
-        console.log('team: ', team);
+          await models.Channel.create({ name: 'general', public: true, teamId: team.id });
+          await models.Channel.create({ name: 'random', public: true, teamId: team.id });
 
-        await models.Channel.create({ name: 'general', public: true, teamId: team.id });
-        await models.Channel.create({ name: 'random', public: true, teamId: team.id });
+          return team;
+        });
 
         return {
           ok: true,
-          team,
+          response,
         };
       } catch (err) {
         console.log(err);
 
         return {
           ok: false,
-          errors: formatErrors(err),
+          errors: formatErrors(err, models),
         };
       }
     }),
