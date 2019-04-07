d7e39dc8e8ac4767563106bc8208f0154cf18f80
Craig Stroman <cstroman@gmail.com>
2019-04-07 16:31:16 -0400

    Starting messages query

--- server/schema/message.js
+++ server/schema/message.js
@@ -4,7 +4,13 @@ export default `
     text: String!
     user: User!
     channel: Channel!
+    createdAt: String!
   }
+
+  type Query {
+    messages(channelId: Int!): [Message!]!
+  }
+
   type Mutation {
     createMessage(channelId: Int!, text: String!): Boolean!
   }
