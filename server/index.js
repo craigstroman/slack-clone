3338dff30f4b35406532c6cb2ce607033fe82636
Craig Stroman <cstroman@gmail.com>
2019-04-21 16:07:55 -0400

    Adding auth to websockets

--- server/index.js
+++ server/index.js
@@ -131,6 +131,32 @@ models.sequelize.sync().then(() => {
         execute,
         subscribe,
         schema,
+        onConnect: async ({ token, refreshToken }, WebSocket) => {
+         if (token && refreshToken) {
+            let user = null;
+
+            try {
+              const payload = jwt.verify(token, SECRET);
+              user = payload.user;
+            } catch (err) {
+              const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+              user = newTokens.user;
+            }
+            if (!user) {
+              throw new Error('Invalid auth tokens');
+            }
+
+            const member = await models.Member.findOne({ where: { teamId: 1, userId: user.id } });
+
+            if (!member) {
+              throw new Error('Missing auth tokens!');
+            }
+
+            return true;
+         }
+
+         throw new Error('Missing auth tokens!');
+        },
       },
       {
         server,
