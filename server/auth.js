e99cd2bc934f940988fda7d0fd746dcc288719be
Craig Stroman <cstroman@gmail.com>
2019-04-27 16:48:26 -0400

    Adding usrname to token

--- server/auth.js
+++ server/auth.js
@@ -13,7 +13,7 @@ import bcrypt from 'bcrypt';
 export const createTokens = async (user, secret, secret2) => {
   const createToken = jwt.sign(
     {
-      user: _.pick(user, ['id']),
+      user: _.pick(user, ['id', 'username']),
     },
     secret,
     {
@@ -88,6 +88,7 @@ export const refreshTokens = async (token, refreshToken, models, SECRET) => {
  */
 export const tryLogin = async (email, password, models, SECRET, SECRET2) => {
   const user = await models.User.findOne({ where: { email }, raw: true });
+
   if (!user) {
     // user with provided email not found
     return {
