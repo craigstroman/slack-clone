8d303c5e2af07a278d8e0b92481ae800d50b6c4d
Craig Stroman <cstroman@gmail.com>
2019-11-07 15:48:11 -0500

    Adding styled components

--- client/App.jsx
+++ client/App.jsx
@@ -1,22 +1,35 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { ApolloProvider } from 'react-apollo';
+import { createGlobalStyle } from 'styled-components';
 import Routes from './routes/routes';
-import './App.scss';
-
 import registerServiceWorker from './registerServiceWorker';
 import client from './apollo';
 
+const GlobalStyles = createGlobalStyle`
+  html,
+  body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    height: 100%;
+    overflow: hidden;
+  }
+`;
+
 const App = (
-  <ApolloProvider client={client}>
-    <Routes />
-  </ApolloProvider>
+  <Fragment>
+    <ApolloProvider client={client}>
+      <Routes />
+    </ApolloProvider>
+    <GlobalStyles />
+  </Fragment>
 );
 
-ReactDOM.render(
-  App,
-  document.getElementById('app'),
-);
+ReactDOM.render(App, document.getElementById('app'));
 
 registerServiceWorker();
 
