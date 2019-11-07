6fcec17ab32878912363dcd2ea974552e7591a85
Craig Stroman <cstroman@gmail.com>
2019-11-07 15:31:29 -0500

    Removing ReactStrap, adding Material UI, and styled components

--- client/components/Header/Header.jsx
+++ client/components/Header/Header.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Dropdown, DropdownToggle, DropdownMenu, DropdownItem,
-} from 'reactstrap';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog, faUserAlt } from '@fortawesome/free-solid-svg-icons';
 import jwt from 'jsonwebtoken';
-import './Header.scss';
+
+const Wrapper = styled.div`
+  height: 10%;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      dropdownOpen: false,
+      anchorEl: null,
     };
 
-    this.toggle = this.toggle.bind(this);
     this.logout = this.logout.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   /**
@@ -31,32 +39,34 @@ class Header extends React.Component {
     localStorage.removeItem('token');
 
     history.push('/');
-  }
+  };
 
-  /**
-   * Toggles the dropdown.
-   *
-   */
-  toggle = () => {
-    const { dropdownOpen } = this.state;
+  handleOpen = e => {
+    const { currentTarget } = e;
 
     this.setState({
-      dropdownOpen: !dropdownOpen,
+      anchorEl: currentTarget,
     });
-  }
+  };
+
+  handleClose = () => {
+    this.setState({
+      anchorEl: null,
+    });
+  };
 
   render() {
-    const { dropdownOpen } = this.state;
+    const { anchorEl } = this.state;
     const { match, channels, users } = this.props;
     const token = jwt.decode(localStorage.getItem('token'));
     let title = '';
 
     if (match.params.channelId) {
-      const channel = channels.filter(el => (el.uuid === match.params.channelId));
+      const channel = channels.filter(el => el.uuid === match.params.channelId);
 
       title = `#${channel[0].name}`;
     } else if (match.params.userId) {
-      const user = users.filter(el => (el.uuid === match.params.userId));
+      const user = users.filter(el => el.uuid === match.params.userId);
 
       if (user[0].id === token.user.id) {
         title = `${user[0].username} (you)`;
@@ -66,45 +76,30 @@ class Header extends React.Component {
     }
 
     return (
-      <div className="header">
-        <div className="row">
-          <div className="col-md-12 text-left">
-            <b>
-              {title}
-            </b>
-          </div>
-        </div>
-        <div className="row">
-          {match.params.channelId && (
-            <div className="col-md-6 text-left">
-              <div className="channel-users">
-                <FontAwesomeIcon icon={faUserAlt} />
-              </div>
-            </div>
-          )}
-          <div className={(match.params.channelId ? 'col-md-6 text-right' : 'col-md-12 text-right')}>
-            <Dropdown isOpen={dropdownOpen} toggle={this.toggle}>
-              <DropdownToggle
-                tag="span"
-                onClick={this.toggle}
-                data-toggle="dropdown"
-                aria-expanded={dropdownOpen}
-              >
-                <FontAwesomeIcon icon={faCog} className="settings-icon" />
-              </DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem>Settings</DropdownItem>
-                <DropdownItem
-                  className="btn btn-link"
-                  onClick={this.logout}
-                >
-                  Logout
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </div>
-        </div>
-      </div>
+      <Wrapper>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <h4>{title}</h4>
+          </Grid>
+          <Grid item xs={6} style={{ textAlign: 'left' }}>
+            {match.params.channelId && <FontAwesomeIcon icon={faUserAlt} />}
+          </Grid>
+          <Grid item xs={6} style={{ textAlign: 'right' }}>
+            <Button aria-controls="settings-menu" aria-haspopup="true" onClick={e => this.handleOpen(e)}>
+              <FontAwesomeIcon icon={faCog} className="settings-icon" />
+            </Button>
+            <Menu
+              id="settings-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={this.handleClose}
+            >
+              <MenuItem onClick={this.logout}>Logout</MenuItem>
+            </Menu>
+          </Grid>
+        </Grid>
+      </Wrapper>
     );
   }
 }
