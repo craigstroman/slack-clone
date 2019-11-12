a221e7f516aba1ed2b74a36c2532f6ccd475ff14
Craig Stroman <cstroman@gmail.com>
2019-11-11 21:58:25 -0500

    Adding Material and removing Bootstrap

--- client/components/NoTeams/NoTeams.jsx
+++ client/components/NoTeams/NoTeams.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: block;
+  width: 100%;
+`;
 
 const NoTeams = () => (
-  <div className="containter">
-    <div className="row">
-      <div className="col-md-12 text-center">
+  <Wrapper>
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
         You need to&nbsp;
-        <a href="/create-team">
-          create a team
-        </a>
-        .
-      </div>
-    </div>
-  </div>
+        <a href="/create-team">create a team</a>.
+      </Grid>
+    </Grid>
+  </Wrapper>
 );
 
 export default NoTeams;
