5fa0d5d7f4369468ad871fbdf15b9970ac26961e
Craig Stroman <cstroman@gmail.com>
2019-11-11 21:59:53 -0500

    Adding styled components

--- client/components/MainSidebar/MainSidebar.jsx
+++ client/components/MainSidebar/MainSidebar.jsx
@@ -1,12 +1,62 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'reactstrap';
+import styled, { ThemeProvider } from 'styled-components';
+import { IconButton } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusCircle, faCircle } from '@fortawesome/free-solid-svg-icons';
 import InvitePeople from '../InvitePeople/InvitePeople';
 import Channels from '../Channels/Channels';
 import DirectMessages from '../DirectMessages/DirectMessages';
-import './MainSidebar.scss';
+import clearFix from '../../shared/themes/mixins';
+import theme from '../../shared/themes';
+
+const Wrapper = styled.div`
+  padding-top: 15px;
+`;
+
+const Header = styled.header`
+  h2 {
+    color: ${props => props.theme.colors.white};
+    text-align: center;
+  }
+`;
+
+const User = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  h6 {
+    color: ${props => props.theme.colors.white};
+    display: inline-block;
+    text-align: center;
+  }
+  svg {
+    color: ${props => props.theme.colors.jungleGreen};
+    font-size: 0.85em;
+    margin-right: 5px;
+  }
+  ${clearFix()}
+`;
+
+const Invite = styled.section`
+  color: ${props => props.theme.colors.shadyLady};
+  display: block;
+  height: 40px;
+  width: 100%;
+  h5,
+  button {
+    float: left;
+  }
+
+  h5 {
+    margin-left: ${props => props.theme.sidebar.marginLeft};
+  }
+
+  button {
+    bottom: 13px;
+    color: ${props => props.theme.colors.shadyLady};
+    margin-left: 10px;
+  }
+`;
 
 class MainSidebar extends React.Component {
   constructor(props) {
@@ -31,38 +81,38 @@ class MainSidebar extends React.Component {
      * Sets which item is selected when component loads.
      */
     if (match.params.channelId) {
-      const channel = channels.filter(el => (el.uuid === match.params.channelId));
+      const channel = channels.filter(el => el.uuid === match.params.channelId);
 
       this.setState({ activeEl: channel[0].uuid });
     } else if (match.params.userId) {
-      const user = teamMembers.filter(el => (el.uuid === match.params.userId));
+      const user = teamMembers.filter(el => el.uuid === match.params.userId);
 
       this.setState({ activeEl: user[0].uuid });
     }
-  }
+  };
 
   /**
    * Handles getting the selected user.
    *
    * @param      {Object}  user    The user.
    */
-  handleGetUser = (user) => {
+  handleGetUser = user => {
     this.setState({ user, activeEl: user.uuid });
-  }
+  };
 
   /**
    * Sets focus to an item selected in the sidebar.
    *
    */
-  handleSelectItem = (e) => {
-    const { target } = e;
-    const { parentNode } = target;
+  handleSelectItem = e => {
+    const { currentTarget } = e;
+    const { parentNode } = currentTarget;
     const elId = parentNode.id;
 
     this.setState({
       activeEl: elId,
     });
-  }
+  };
 
   /**
    * Opens the invite people modal.
@@ -70,7 +120,7 @@ class MainSidebar extends React.Component {
    */
   handleOpenInvitePeople = () => {
     this.setState({ invitePeopleModal: true });
-  }
+  };
 
   /**
    * Closes the invite people modal.
@@ -78,71 +128,67 @@ class MainSidebar extends React.Component {
    */
   handleCloseInvitePeople = () => {
     this.setState({ invitePeopleModal: false });
-  }
+  };
 
   render() {
-    const {
-      channels, directMessageUsers, teamName, teamId, teamUUID, currentUser, isOwner,
-    } = this.props;
+    const { channels, directMessageUsers, teamName, teamId, teamUUID, currentUser, isOwner } = this.props;
     const { activeEl, invitePeopleModal, user } = this.state;
 
     return (
-      <div className="main-sidebar-container">
-        <header>
-          <h2 className="main-sidebar__title">{teamName}</h2>
-          <div className="main-sidebar__user">
-            <FontAwesomeIcon icon={faCircle} className="user-status" />
-            <h6 className="main-sidebar__username">
-              {currentUser.username}
-            </h6>
-          </div>
-        </header>
-        <section>
-          <Fragment>
-            <Channels
-              isOwner={isOwner}
-              channels={channels}
-              activeEl={activeEl}
-              teamId={teamId}
-              teamUUID={teamUUID}
-              selectItem={this.handleSelectItem}
-              {...this.props}
-            />
-          </Fragment>
-          <Fragment>
-            <DirectMessages
-              directMessageUsers={directMessageUsers}
-              user={currentUser}
-              messageUser={user}
-              activeEl={activeEl}
-              selectItem={this.handleSelectItem}
-              handleGetUser={this.handleGetUser}
-              teamId={teamId}
-              teamUUID={teamUUID}
-              {...this.props}
-            />
-          </Fragment>
-          {isOwner && (
-            <div className="invite-people">
-              <div className="sidebar-heading">
+      <ThemeProvider theme={theme}>
+        <Wrapper>
+          <Header>
+            <h2>{teamName}</h2>
+            <User>
+              <FontAwesomeIcon icon={faCircle} className="user-status" />
+              <h6>{currentUser.username}</h6>
+            </User>
+          </Header>
+          <section>
+            <Fragment>
+              <Channels
+                isOwner={isOwner}
+                channels={channels}
+                activeEl={activeEl}
+                teamId={teamId}
+                teamUUID={teamUUID}
+                selectItem={this.handleSelectItem}
+                {...this.props}
+              />
+            </Fragment>
+            <Fragment>
+              <DirectMessages
+                directMessageUsers={directMessageUsers}
+                user={currentUser}
+                messageUser={user}
+                activeEl={activeEl}
+                selectItem={this.handleSelectItem}
+                handleGetUser={this.handleGetUser}
+                teamId={teamId}
+                teamUUID={teamUUID}
+                {...this.props}
+              />
+            </Fragment>
+            {isOwner && (
+              <Invite>
                 <h5>Invite People To Join</h5>
-                <Button
+                <IconButton
                   type="button"
                   className="sidebar-heading__action"
                   onClick={this.handleOpenInvitePeople}
                 >
                   <FontAwesomeIcon icon={faPlusCircle} />
-                </Button>
-              </div>
-            </div>
-          )}
-        </section>
-        <InvitePeople
-          isOpen={invitePeopleModal}
-          teamId={teamId}
-          handleCloseInvitePeople={() => this.handleCloseInvitePeople()}
-        />
-      </div>
+                </IconButton>
+              </Invite>
+            )}
+          </section>
+          <InvitePeople
+            isOpen={invitePeopleModal}
+            teamId={teamId}
+            handleCloseInvitePeople={() => this.handleCloseInvitePeople()}
+          />
+        </Wrapper>
+      </ThemeProvider>
     );
   }
 }
