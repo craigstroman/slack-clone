7ade9391328101063c482f96b48721755346b9bf
Craig Stroman <cstroman@gmail.com>
2019-11-18 22:09:47 -0500

    Adding styled components

--- client/components/TeamSidebar/TeamSidebar.jsx
+++ client/components/TeamSidebar/TeamSidebar.jsx
@@ -1,6 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled, { ThemeProvider } from 'styled-components';
+import { Button } from '@material-ui/core';
 import './TeamSidebar.scss';
+import theme from '../../shared/themes';
+import clearFix from '../../shared/themes/mixins';
+
+const Wrapper = styled.div`
+  color: ${props => props.theme.colors.white};
+`;
+
+const TeamsList = styled.ul`
+  list-style: none;
+  padding-left: 0px;
+  width: 100%;
+  li {
+    display: flex;
+    margin-bottom: 20px;
+    margin-left: auto;
+    margin-right: auto;
+    &:first-child {
+      margin-top: 10px;
+    }
+    button {
+      background-color: #676066;
+      border-radius: 11px;
+      cursor: pointer;
+      color: ${props => props.theme.colors.white};
+      font-size: 24px;
+      height: 50px;
+      margin-bottom: 10px;
+      margin-left: auto;
+      margin-right: auto;
+      width: 50px;
+      &:hover,
+      &:focus,
+      &:active,
+      &:visited {
+        background-color: #676066;
+        border-style: solid;
+        border-width: thick;
+        border-color: #767676;
+        color: ${props => props.theme.colors.white};
+        text-decoration: none;
+      }
+    }
+  }
+`;
 
 class TeamSidebar extends React.Component {
   constructor(props) {
@@ -9,50 +55,44 @@ class TeamSidebar extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick = (e) => {
+  handleClick = e => {
     const { handleChangeTeam, history } = this.props;
+    const { currentTarget } = e;
 
-    const teamName = e.target.getAttribute('team');
-    const teamId = e.target.getAttribute('teamid');
-    const teamUUID = e.target.getAttribute('teamuuid');
+    const teamName = currentTarget.getAttribute('team');
+    const teamId = currentTarget.getAttribute('teamid');
+    const teamUUID = currentTarget.getAttribute('teamuuid');
 
     history.push(`/dashboard/view/team/${teamUUID}`);
 
-    handleChangeTeam(teamName, teamId);
-  }
+    handleChangeTeam(teamName, teamId, teamUUID);
+  };
 
   render() {
     const { userTeams } = this.props;
 
     return (
-      <div className="teams-sidebar-container">
-        <section>
-          <div className="teams-sidebar__content">
-            <ul className="teams-list">
-              {userTeams.map(el => (
-                <li
-                  className="teams-list__item"
-                  id={`teams-${el.id}`}
-                  key={`teams-${el.uuid}`}
-                  type={el.type}
+      <ThemeProvider theme={theme}>
+        <Wrapper>
+          <TeamsList>
+            {userTeams.map(el => (
+              <li id={`teams-${el.id}`} key={`teams-${el.uuid}`} type={el.type}>
+                <Button
+                  type="button"
+                  className="team-item"
+                  title={el.name}
+                  team={el.name}
+                  teamid={el.id}
+                  teamuuid={el.uuid}
+                  onClick={e => this.handleClick(e)}
                 >
-                  <button
-                    type="button"
-                    className="team-item"
-                    title={el.name}
-                    team={el.name}
-                    teamid={el.id}
-                    teamuuid={el.uuid}
-                    onClick={e => this.handleClick(e)}
-                  >
-                    { el.letter }
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </section>
-      </div>
+                  {el.letter}
+                </Button>
+              </li>
+            ))}
+          </TeamsList>
+        </Wrapper>
+      </ThemeProvider>
     );
   }
 }
