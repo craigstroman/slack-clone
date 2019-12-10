ad63c926fc02ed5c8398ad563e7894d7cb38e824
Craig Stroman <cstroman@gmail.com>
2019-12-10 16:26:48 -0500

    Adding route for teams page

--- client/routes/routes.jsx
+++ client/routes/routes.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter, Route, Switch,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PrivateRoute from '../shared/util/privateRoutes';
 
 import Home from '../pages/Home/Home';
@@ -9,6 +7,7 @@ import Dashboard from '../pages/Dashboard/Dashboard';
 import Register from '../pages/Register/Register';
 import Login from '../pages/Login/Login';
 import CreateTeam from '../components/CreateTeam/CreateTeam';
+import Teams from '../pages/Teams/Teams';
 
 const Routes = () => (
   <BrowserRouter>
@@ -16,6 +15,7 @@ const Routes = () => (
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
+      <PrivateRoute path="/teams" exact component={Teams} />
       <PrivateRoute path="/dashboard" exact component={Dashboard} />
       <PrivateRoute path="/dashboard/view/team/:teamId?" exact component={Dashboard} />
       <PrivateRoute path="/dashboard/view/team/:teamId?/channel/:channelId?" exact component={Dashboard} />
