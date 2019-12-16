5d0d4bcd0c61fa0b23bccbd03debb8b6cdc7c541
Craig Stroman <cstroman@gmail.com>
2019-12-16 13:18:23 -0500

    Converting to styled components

--- client/components/CreateTeam/CreateTeam.jsx
+++ client/components/CreateTeam/CreateTeam.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'react-apollo';
-import {
-  Button, Col, Form, FormGroup, Label, Input,
-} from 'reactstrap';
+// import { Button, Col, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Grid, TextField } from '@material-ui/core';
+import styled, { ThemeProvider } from 'styled-components';
 import gql from 'graphql-tag';
-import './CreateTeam.scss';
+import theme from '../../shared/themes';
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  header {
+    h1 {
+      text-align: center;
+    }
+  }
+  main {
+    form {
+      margin: 0 auto;
+      text-align: center;
+    }
+  }
+`;
+
+const StyledTextField = styled(TextField)`
+  .MuiOutlinedInput-root {
+    text-align: left;
+    width: 500px;
+  }
+`;
 
 class CreateTeam extends React.Component {
   constructor(props) {
@@ -13,18 +36,42 @@ class CreateTeam extends React.Component {
 
     this.state = {
       name: '',
-      errors: false,
+      fieldErrors: '',
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.validateForm = this.validateForm.bind(this);
   }
 
-  handleChange = (e) => {
+  validateForm = () => {
+    const { name } = this.state;
+    const errors = {};
+
+    if (!name.length) {
+      errors.name = 'Team name is required.';
+    }
+
+    if (Object.keys(errors).length >= 1) {
+      this.setState({
+        fieldErrors: errors,
+      });
+
+      return false;
+    }
+
+    this.setState({
+      fieldErrors: errors,
+    });
+
+    return true;
+  };
+
+  handleChange = e => {
     const { value } = e.target;
 
     this.setState({ name: value });
-  }
+  };
 
   handleSubmit = async () => {
     const { name } = this.state;
@@ -52,58 +99,55 @@ class CreateTeam extends React.Component {
         this.errors = err;
       }
     }
-  }
+  };
 
   render() {
-    const { name, errors } = this.state;
+    const { name, errors, fieldErrors } = this.state;
+
+    console.log('fieldErrors: ', fieldErrors);
 
     return (
-      <div className="container">
-        <header>
-          <div className="header">
-            <h1 className="text-center">
-              Create A Team
-            </h1>
-          </div>
-        </header>
-        <main>
-          <div className="content">
-            <div
-              className={errors ? 'error visible' : 'error'}
+      <ThemeProvider theme={theme}>
+        <Wrapper>
+          <header>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <h1>Slack Clone - Create A Team</h1>
+                <hr />
+              </Grid>
+            </Grid>
+          </header>
+          <main>
+            <form
+              onSubmit={e => {
+                e.preventDefault();
+              }}
             >
-              <div className="alert alert-danger">
-                Invalid team name.
-              </div>
-            </div>
-            <Form onSubmit={(e) => { e.preventDefault(); }}>
-              <FormGroup row>
-                <Label for="name" md={2}>
-                  Team Name:
-                </Label>
-                <Col md={10}>
-                  <Input
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <StyledTextField
+                    label="Team Name *"
                     type="text"
                     name="name"
-                    value={name}
+                    margin="normal"
+                    variant="outlined"
                     onChange={e => this.handleChange(e)}
+                    onBlur={this.validateForm}
+                    error={!fieldErrors.name === false}
+                    helperText={fieldErrors.name}
+                    value={name}
                   />
-                </Col>
-              </FormGroup>
-              <div className="row">
-                <div className="col-md-12 text-center">
-                  <Button
-                    type="submit"
-                    color="primary"
-                    onClick={() => this.handleSubmit()}
-                  >
+                </Grid>
+                <Grid item xs={12}>
+                  <Button type="button" variant="contained" color="primary" onClick={this.handleSubmit}>
                     Create Team
                   </Button>
-                </div>
-              </div>
-            </Form>
-          </div>
-        </main>
-      </div>
+                </Grid>
+              </Grid>
+            </form>
+          </main>
+        </Wrapper>
+      </ThemeProvider>
     );
   }
 }
