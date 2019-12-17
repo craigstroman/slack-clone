e13d703971505c0d73bd08380a6f071988035dc0
Craig Stroman <cstroman@gmail.com>
2019-12-17 12:00:46 -0500

    Adding more global styles

--- client/App.jsx
+++ client/App.jsx
@@ -30,6 +30,30 @@ const GlobalStyles = createGlobalStyle`
     margin-block-end: 0;
 
   }
+
+  h1 {
+    font-size: 2em;
+  }
+
+  h2 {
+    font-size: 1.5em;
+  }
+
+  h3 {
+    font-size: 1.17em;
+  }
+
+  h4 {
+    font-size: 1em;
+  }
+
+  h5 {
+    font-size: .83em;
+  }
+
+  h6 {
+    font-size: .67em;
+  }
 `;
 
 const App = (
