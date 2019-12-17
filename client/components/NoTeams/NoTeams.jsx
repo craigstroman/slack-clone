6207b0e1126605553bc2b20ed2602a8bd33f9640
Craig Stroman <cstroman@gmail.com>
2019-12-17 12:59:26 -0500

    Updating content

--- client/components/NoTeams/NoTeams.jsx
+++ client/components/NoTeams/NoTeams.jsx
@@ -3,18 +3,37 @@ import { Grid } from '@material-ui/core';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  display: block;
+  margin: 0 auto;
   width: 100%;
+  header {
+    h1 {
+      text-align: center;
+    }
+  }
+  main {
+    margin: 0 auto;
+    text-align: center;
+  }
 `;
 
 const NoTeams = () => (
   <Wrapper>
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        You need to&nbsp;
-        <a href="/create-team">create a team</a>.
+    <header>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <h1>Slack Clone - No Teams</h1>
+          <hr />
+        </Grid>
       </Grid>
-    </Grid>
+    </header>
+    <main>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          You need to&nbsp;
+          <a href="/create-team">create a team</a>.
+        </Grid>
+      </Grid>
+    </main>
   </Wrapper>
 );
 
