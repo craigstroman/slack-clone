b64530a1ab1f8a794e51fcff7fa110c880c58e7b
Craig Stroman <cstroman@gmail.com>
2019-12-30 11:32:26 -0500

    Fixing direct message subscription

--- server/resolvers/directMessage.js
+++ server/resolvers/directMessage.js
@@ -9,17 +9,20 @@ const NEW_DIRECT_MESSAGE = 'NEW_DIRECT_MESSAGE';
 export default {
   Subscription: {
     newDirectMessage: {
-      subscribe: directMessageSubscription.createResolver(withFilter(
-        () => pubsub.asyncIterator(NEW_DIRECT_MESSAGE),
-        (payload, args, { user }) =>
-          payload.teamId === args.teamId &&
-            ((payload.senderId === user.id && payload.receiverId === args.userId) ||
-              (payload.senderId === args.userId && payload.receiverId === user.id)),
-      )),
-    }
+      subscribe: directMessageSubscription.createResolver(
+        withFilter(
+          () => pubsub.asyncIterator(NEW_DIRECT_MESSAGE),
+          (payload, args, { user }) =>
+            payload.teamId === args.teamId &&
+            (payload.senderId === user.id ||
+              payload.receiverId === args.userId ||
+              (payload.senderId === args.userId || payload.receiverId === user.id)),
+        ),
+      ),
+    },
   },
   DirectMessage: {
-    sender: ({ sender, senderId }, args, {models}) => {
+    sender: ({ sender, senderId }, args, { models }) => {
       if (sender) {
         return sender;
       }
@@ -77,6 +80,6 @@ export default {
         console.log(err);
         return false;
       }
-    })
-  }
-}
+    }),
+  },
+};
