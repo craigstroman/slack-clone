09d4b43d59c4a17673941cbf78691ad26c8194ab
Craig Stroman <cstroman@gmail.com>
2020-01-03 15:08:43 -0500

    Moving global styles to external file

--- client/App.jsx
+++ client/App.jsx
@@ -5,60 +5,7 @@ import { createGlobalStyle } from 'styled-components';
 import Routes from './routes/routes';
 import registerServiceWorker from './registerServiceWorker';
 import client from './apollo';
-
-const GlobalStyles = createGlobalStyle`
-  html,
-  body {
-    font-family: arial, helvetica, sans-serif;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
-
-  #app {
-    height: 100%;
-    overflow: hidden;
-  }
-
-  hr {
-    width: 95%;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    margin-block-start: 0;
-    margin-block-end: 0;
-
-  }
-
-  h1 {
-    font-size: 2em;
-  }
-
-  h2 {
-    font-size: 1.5em;
-  }
-
-  h3 {
-    font-size: 1.17em;
-  }
-
-  h4 {
-    font-size: 1em;
-  }
-
-  h5 {
-    font-size: .83em;
-  }
-
-  h6 {
-    font-size: .67em;
-  }
-`;
+import GlobalStyles from './globalStyles';
 
 const App = (
   <Fragment>
