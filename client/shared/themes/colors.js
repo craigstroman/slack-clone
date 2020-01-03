1a05b72b99d06da7e4aab4c1cbf4249a707e1ba3
Craig Stroman <cstroman@gmail.com>
2020-01-03 15:09:49 -0500

    Moving them colors to a file

---
+++ client/shared/themes/colors.js
@@ -0,0 +1,21 @@
+const colors = {
+  white: '#fff',
+  black: '#000',
+  red: '#f00',
+
+  matterhorn: '#4D384B',
+
+  // Team sidebar colors
+  thunder: '#362233',
+
+  // Main sidebar colors
+  shadyLady: '#B6ADB5',
+  valentino: '#3F0E40',
+  toryBlue: '#1164A3',
+  jungleGreen: '#2BAC76',
+
+  // Message colors
+  scorpion: '#616061',
+};
+
+export default colors;
