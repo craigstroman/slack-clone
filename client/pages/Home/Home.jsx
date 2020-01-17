ed0dc0c384fd7e6315bcd221c28ad99d474c451c
Craig Stroman <cstroman@gmail.com>
2020-01-17 15:51:19 -0500

    Moving routes to Home

--- client/pages/Home/Home.jsx
+++ client/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { CssBaseline, Grid } from '@material-ui/core';
+import PrivateRoute from '../../shared/util/privateRoutes';
 import Login from '../Login/Login';
+import Register from '../Register/Register';
+import CreateTeam from '../Teams/CreateTeam/CreateTeam';
+import ViewTeam from '../Teams/ViewTeam/ViewTeam';
+import Teams from '../Teams/Teams/Teams';
 
 const Header = styled.header`
   margin-top: 10px;
@@ -12,22 +18,30 @@ const Header = styled.header`
 
 const Home = props => (
   <Fragment>
-    <CssBaseline />
-    <Header>
-      <h1>Slack Clone</h1>
-      <hr />
-    </Header>
-    <main>
-      <Grid container spacing={3} justify="center">
-        <Grid item lg={12}>
-          <Login {...props} />
+    <BrowserRouter>
+      <CssBaseline />
+      <main>
+        <Grid container spacing={3} justify="center">
+          <Grid item lg={12}>
+            <Switch>
+              <Route path="/" exact component={Login} />
+              <Route path="/login" exact component={Login} />
+              <Route path="/register" exact component={Register} />
+              <PrivateRoute path="/teams" exact component={Teams} />
+              <PrivateRoute path="/dashboard" exact component={ViewTeam} />
+              <PrivateRoute path="/dashboard/view/team/:teamId?" exact component={ViewTeam} />
+              <PrivateRoute
+                path="/dashboard/view/team/:teamId?/channel/:channelId?"
+                exact
+                component={ViewTeam}
+              />
+              <PrivateRoute path="/dashboard/view/team/:teamId?/user/:userId?" exact component={ViewTeam} />
+              <PrivateRoute path="/create-team" exact component={CreateTeam} />
+            </Switch>
+          </Grid>
         </Grid>
-        <Grid item lg={12} style={{ textAlign: 'center' }}>
-          Not a registered user?&nbsp;
-          <a href="/register">Sign Up</a>
-        </Grid>
-      </Grid>
-    </main>
+      </main>
+    </BrowserRouter>
   </Fragment>
 );
 
