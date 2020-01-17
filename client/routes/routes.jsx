01128119353b8153db3eadc7cfa545cf978f3b2e
Craig Stroman <cstroman@gmail.com>
2020-01-17 12:54:21 -0500

    Updating routes

--- client/routes/routes.jsx
+++ client/routes/routes.jsx
@@ -3,10 +3,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PrivateRoute from '../shared/util/privateRoutes';
 
 import Home from '../pages/Home/Home';
-import Dashboard from '../pages/Dashboard/Dashboard';
 import Register from '../pages/Register/Register';
-import CreateTeam from '../pages/Teams/CreateTeam';
-import Teams from '../pages/Teams/Teams';
+import CreateTeam from '../pages/Teams/CreateTeam/CreateTeam';
+import ViewTeam from '../pages/Teams/ViewTeam/ViewTeam';
+import Teams from '../pages/Teams/Teams/Teams';
 
 const Routes = () => (
   <BrowserRouter>
@@ -15,10 +15,10 @@ const Routes = () => (
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Home} />
       <PrivateRoute path="/teams" exact component={Teams} />
-      <PrivateRoute path="/dashboard" exact component={Dashboard} />
-      <PrivateRoute path="/dashboard/view/team/:teamId?" exact component={Dashboard} />
-      <PrivateRoute path="/dashboard/view/team/:teamId?/channel/:channelId?" exact component={Dashboard} />
-      <PrivateRoute path="/dashboard/view/team/:teamId?/user/:userId?" exact component={Dashboard} />
+      <PrivateRoute path="/dashboard" exact component={ViewTeam} />
+      <PrivateRoute path="/dashboard/view/team/:teamId?" exact component={ViewTeam} />
+      <PrivateRoute path="/dashboard/view/team/:teamId?/channel/:channelId?" exact component={ViewTeam} />
+      <PrivateRoute path="/dashboard/view/team/:teamId?/user/:userId?" exact component={ViewTeam} />
       <PrivateRoute path="/create-team" exact component={CreateTeam} />
     </Switch>
   </BrowserRouter>
